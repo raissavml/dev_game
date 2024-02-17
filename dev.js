6eecdab3c0db52549e234a71663b8c8148c39979
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-02-17 19:28:15 +0300

    Update dev.js
    adding onend function

--- dev.js
+++ dev.js
@@ -324,6 +324,9 @@ let sfx = {
   boost: new Howl({
     src: ["https://assets.codepen.io/21542/howler-sfx-levelup.mp3"],
     loop: false,
+    onend: function () {
+      console.log("Done playing sfx!");
+    },
   }),
 };
 
